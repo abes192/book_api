6c65915b9e5d246b000c6db6bde518888e099ac5
ALAN BAYU <115530771+abes192@users.noreply.github.com>
2024-10-30 14:05:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Konfigurasi MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://alan:alan@localhost:3306/db_mvc_api'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flaskapi_therelive:5d8d745bbae0e2e01fb49a2cf2b839432be4904d@q7yjs.h.filess.io:3306/flaskapi_therelive'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
